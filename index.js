ae2eac10c9dba35ddb7488bb24b0dc92bbf26c31
Nasiful Islam Nasif <nasifulislamnasif23@gmail.com>
2023-11-09 19:47:58 +0600

    solved sort related problem

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ async function run() {
                 console.log(sortField, sortOrder)
                 // const quantity1 = { $gt: 0 }
                 // sortByPositive['quantity'] = { $ne: 0 };
-                // sortByPositive = { quantity: { $gt: 0 } }
+                sortByPositive = { quantity: { $gt: 0 } }
                 
             }
             console.log(sortByPositive)
